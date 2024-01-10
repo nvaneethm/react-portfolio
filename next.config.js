dc9bb39d6e5babadc71a256b057c950c31cd4ad8
Navaneeth <navaneeth.murukesh@deltatre.com>
2024-01-10 14:35:54 +0530

    ci/cd build

--- next.config.js
+++ next.config.js
@@ -1,6 +1,32 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-    assetPrefix: process.env.NODE_ENV === 'production' ? '/react-portfolio' : '',
-}
+const isProd = process.env.NODE_ENV === 'production';
 
-module.exports = nextConfig
+module.exports = {
+    // Asset Prefix
+    assetPrefix: isProd ? '/react-portfolio/' : '',
+
+    // Output Configuration
+    output: 'export',
+
+    // For routing with GitHub Pages
+    basePath: isProd ? '/react-portfolio' : '',
+    trailingSlash: true,
+
+    // Next.js Image and CSS Configuration
+    images: {
+        loader: 'imgix', // change to your preferred loader
+        path: isProd ? 'https://nvaneethm.github.io/react-portfolio/' : '',
+    },
+
+    webpack: (config, { isServer }) => {
+        if (!isServer) {
+            // Fixes npm packages that depend on `fs` module
+            config.resolve.fallback = { fs: false };
+        }
+
+        // Further custom webpack configuration can be defined here
+
+        return config;
+    },
+};
+
