e5320f514b2be7089660ca9652e1a393e550d49a
Navaneeth <navaneeth.murukesh@deltatre.com>
2024-01-10 15:37:51 +0530

    path revert

--- next.config.js
+++ next.config.js
@@ -3,13 +3,13 @@ const isProd = process.env.NODE_ENV === 'production';
 
 module.exports = {
     // Asset Prefix
-    assetPrefix: "", //isProd ? '/react-portfolio/' : '',
+    assetPrefix: isProd ? '/react-portfolio/' : '',
 
     // Output Configuration
     output: 'export',
 
     // For routing with GitHub Pages
-    basePath: "", //isProd ? '/react-portfolio' : '',
+    basePath: isProd ? '/react-portfolio' : '',
     trailingSlash: true,
 
     // Next.js Image and CSS Configuration
