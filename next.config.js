af6b591b74134125c640dae98c130e57e65521bd
Navaneeth <navaneeth.murukesh@deltatre.com>
2024-01-17 00:14:42 +0530

    ci/cd asset prefix update

--- next.config.js
+++ next.config.js
@@ -7,11 +7,11 @@ const repoName = 'react-portfolio'; // Replace with your actual repo name if dif
 
 module.exports = {
     // Asset Prefix
-    // assetPrefix: isProd ? `/${repoName}` : '',
+    assetPrefix: isProd ? `./` : '',
     output: 'export',
 
     // For routing with GitHub Pages
-    basePath: isProd ? `/${repoName}` : '',
+    // basePath: isProd ? `/` : '',
     trailingSlash: true,
 
     // Next.js Image and CSS Configuration
