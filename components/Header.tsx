5726ee52b6adc3f1929e1e88f9b9be82e6ae2c65
Navaneeth <navaneeth.murukesh@deltatre.com>
2024-01-17 18:29:17 +0530

    navbar basic styling

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,14 +1,14 @@
 // components/Header.tsx
 // import Navbar from './Navbar';
-import styles from '../styles/Header.module.css';
-import Navbar from './Navbar';
+import styles from "../styles/Header.module.css";
+import Navbar from "./Navbar";
 
 const Header = () => {
-    return (
-        <header className={styles.header}>
-            <Navbar></Navbar>
-        </header>
-    );
-}
+  return (
+    <header className={styles.header}>
+      <Navbar></Navbar>
+    </header>
+  );
+};
 
 export default Header;
